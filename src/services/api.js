d0486ab9ed61d2aa9a9e39538cd4d8a2c148d131
nesd4rk <nesd4rkfocus@gmail.com>
2023-12-17 19:13:51 -0300

    baseUrl updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:7070',
+  // baseURL: 'http://localhost:7070',
+  baseURL: 'https://products-inventory-api.onrender.com',
 });
