f6f9795866b367158f949fd5c299ec973df0545b
Christian Perez <nesd4rkfocus@gmail.com>
2023-12-26 03:38:38 -0300

    fixed vercel

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  // baseURL: 'http://15.229.71.122',
-  baseURL: 'https://jbc-inventory-hdkq46puk-nesdark.vercel.app/',
+  // baseURL: 'http://localhost:7070',
+  baseURL: 'https://jbc-inventory.vercel.app/',
 });
