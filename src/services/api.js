4a4b81292da75cfa070518d9e2f73f5bf9d235ef
Christian Perez <nesd4rkfocus@gmail.com>
2023-12-27 00:34:08 -0300

    Reverted to Update BaseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  // baseURL: 'http://localhost:7070',
-  baseURL: 'http://191.101.71.70:3000/',
+  baseURL: 'http://191.101.71.70:3000',
 });
