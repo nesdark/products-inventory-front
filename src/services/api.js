a6219a9df9bc661eb7a9ce2be1e6bcd2f109c106
Christian Perez <nesd4rkfocus@gmail.com>
2024-01-08 21:34:02 -0300

    save commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://15.229.71.122:6060',
-  // baseURL: 'http://localhost:6060',
+  // baseURL: 'http://15.229.71.122:6060',
+  baseURL: 'http://localhost:6060',
 });
 
 // npm run dev -- --host --port 80
